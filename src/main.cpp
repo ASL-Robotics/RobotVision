d2af838604760a437fc4dccfa89d19cc5a7f88a1
Will V <will_vauclain@asl.org>
2016-09-07 21:14:06 +0100

    Added Support For Filtering Contours

--- src/main.cpp
+++ src/main.cpp
@@ -29,9 +29,17 @@ int main(int argc, char **argv) {
   hsvhigh1 = Mat(image.size(), image.type(), range1.maxValues());
   hsvlow2 = Mat(image.size(), image.type(), range2.minValues());
   hsvhigh2 = Mat(image.size(), image.type(), range2.maxValues());
+
+  Mat filtered = findRetroreflectiveTape(image);
   
-  namedWindow("Display Image", WINDOW_AUTOSIZE );
-  imshow("Display Image", findRetroreflectiveTape(image));
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  findContours( filtered, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
+  vector<int> filteredContours = filterContours(filtered, contours, hierarchy);
+
+  renderSelectContours(Mat(image.size(), image.type(), Scalar(0,0,0,0)), contours, hierarchy, filteredContours);
+
   waitKey(0);
   return 0;
 }
@@ -56,4 +64,55 @@ Mat bitwiseOr(Mat input1, Mat input2) {
   Mat out(input1.size(), input1.type(), Scalar(0,0,0,0));
   bitwise_or(input1, input2, out);
   return out;
+}
+
+void renderAllContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy) {
+    // iterate through all the top-level contours,
+    // draw each connected component with its own random color
+    int idx = 0;
+    for( ; idx >= 0; idx = hierarchy[idx][0] )
+    {
+        Scalar color( rand()&255, rand()&255, rand()&255 );
+        drawContours( src, contours, idx, color, CV_FILLED, 8, hierarchy );
+    }
+
+    namedWindow( "Components", 1 );
+    imshow( "Components", src );
+}
+
+void renderSelectContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy, vector<int> idxs) {
+    // iterate through all the top-level contours,
+    // draw each connected component with its own random color
+    for(int idx: idxs)
+    {
+        Scalar color( rand()&255, rand()&255, rand()&255 );
+        drawContours( src, contours, idx, color, CV_FILLED, 8, hierarchy );
+    }
+
+    namedWindow( "Components", 1 );
+    imshow( "Components", src );
+}
+
+vector<int> filterContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy) {
+    // iterate through all the top-level contours,
+    // filter each one
+    vector<int> idxs;
+    Rect bb;
+    double area;
+
+    int idx = 0;
+    for( ; idx >= 0; idx = hierarchy[idx][0] )
+    {
+        bb = boundingRect(contours[idx]);
+        area = contourArea(contours[idx]);
+
+        if(area < MIN_AREA)
+            continue;
+        
+        idxs.push_back(idx);
+        std::cout << bb << std::endl;
+        std::cout << area << std::endl;
+    }
+
+    return idxs;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include "hsvrange.hpp"
+
 using namespace cv;
+
+#define MIN_AREA 1500
 
 Mat hsvFilter(Mat, Mat, Mat);
 Mat bitwiseOr(Mat, Mat);
 Mat findRetroreflectiveTape(Mat);
+void renderAllContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy);
+void renderSelectContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy, vector<int> idxs);
+vector<int> filterContours(Mat src, vector< vector<Point> > contours, vector<Vec4i> hierarchy);
